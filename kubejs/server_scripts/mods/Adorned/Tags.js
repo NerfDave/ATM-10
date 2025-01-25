ff958a154ba84c67ceb1ccf63a931f5bb2c7ed58
ItsDave <80086319+NerfDave@users.noreply.github.com>
2025-01-25 04:13:50 -0800

    Update Tags.js
    Allows Apothic Attributes' Potion charms to be equipped in Curios Charm Slots

--- kubejs/server_scripts/mods/Adorned/Tags.js
+++ kubejs/server_scripts/mods/Adorned/Tags.js
@@ -5,6 +5,7 @@ ServerEvents.tags('item', allthemods => {
     allthemods.add('curios:ring', 'evilcraft:vengeance_ring')
     allthemods.add('curios:charm', 'evilcraft:invigorating_pendant')
     allthemods.add('curios:charm', 'evilcraft:primed_pendant')
+    allthemods.add('curios:charm', 'apotheosis:potion_charm')
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10
